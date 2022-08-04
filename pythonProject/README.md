bbf44fbe931fe076efe1e2a936386a89e027f9bc
Witchho <vinkler.prodej@gmail.com>
2022-08-04 15:28:20 +0200

    Initial commit

---
+++ pythonProject/README.md
@@ -0,0 +1,24 @@
+WEB SCRAPING
+Tento projekt je založení na web scraping 
+volby.cz z roku 2017.
+
+Scrapinguje data o volbách a posléze tyto data ukládá do csv souboru.
+
+Uživatel si vybere:
+1. Uzemní celek
+2. Podoblast územního celku
+
+
+
+Instalace knihoven:
+Na stránce https://www.tutorialspoint.com/python/python_modules.htm , lze nalézt popis určených knihoven
+
+Knihovny lze instalovat 2 způsoby:
+1. V terminále
+$pip install 'jméno balíčku'  (beautifulsoup4)
+2. PyCharm -> File -> New Project Setup -> Preferences for New Project
+, Po vybrání Python Interpreter můžu pomoci symbolu '+' instalovat balíčky
+
+Příklad fungování:
+Po přivítání uživatele, bude uživatel dotázán, do jaké oblasti chce uživatel nahlížet a posléze jaké data ho zajímají (Všechny nebo jenom počet volících
+)
---
+++ pythonProject/main.py
@@ -0,0 +1,72 @@
+'''
+Projekt_3_Engeto
+Autor: Ondřej Vinkler
+Email: vinkler.prodej@gmail.com
+Github: https://github.com/Wiitchho
+'''
+from pprint import pprint
+import csv
+import os
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+
+def filtr(url):
+    resp = requests.get(url)
+    soup = bs(resp.text, 'html.parser')
+    return soup
+
+def take_url(soup):
+    hrefs = []
+    for td in soup.find_all('td'):
+        aa = td.find_all('a', href=True)
+    # obsahuje to `href`
+        if len(aa) > 0:
+            a = aa[0]
+            hrefs.append(a['href'])
+    # neobsahuje to `href`, preskocime to
+        else:
+            continue
+    return hrefs
+
+def scrap_next(url):
+    seznam_o = []
+    odkaz = requests.get(url)
+    text = bs(odkaz.text,'html.parser')
+    for tr in text.find_all('tr'):
+        aa = tr.find_all('a', href=True)
+        if len(aa) >0:
+            a = aa[0]
+            seznam_o.append('https://volby.cz/pls/ps2017nss/' + a['href'])
+    return seznam_o
+
+
+pprint(scrap_next('https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8106'))
+
+
+
+def add_all_url(hrefs):
+    all_hrefs = []
+    for href in hrefs:
+        all_hrefs.append('https://volby.cz/pls/ps2017nss/' + href)
+    return all_hrefs
+
+#pprint(add_all_url(take_url(filtr("https://volby.cz/pls/ps2017nss/ps3?xjazyk=CZ"))))
+
+def second_pars(url2):
+    hotovo = filtr(url2)
+    for tag in hotovo.find_all("td"):
+        print(tag)
+
+def browser(final_odkaz):
+    hledam_cesty = final_odkaz.find_all('td',{'class':'cislo'})
+    sezna_m =[]
+    for d_ata in hledam_cesty:
+        sezna_m.append(d_ata)
+    pprint(sezna_m)
+#second_pars('https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8106')
+
+
+
+
